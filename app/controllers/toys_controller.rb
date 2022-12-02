ceb69f8ae19e64f75ee80b17d3ecbe3b4531aa89
martin mithi <gathitumithi9000@gmail.com>
2022-12-02 10:13:54 +0300

    commit tree 5fcb08ff9132c98c147d5b49612e03d56bac962d

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -3,23 +3,24 @@ class ToysController < ApplicationController
 
   def index
     toys = Toy.all
-    render json: toys
+    render json: toys, status: :ok
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy.update(likes: toy.likes + 1)
+    render json: toy
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
-    head :no_content
+    render json: toy
   end
 
   private
